e2dd8fb9be2a1bf66120654d54323d76eb2bc781
Earl Hans Genoso <earlhansg@gmail.com>
2024-01-27 02:30:33 +0800

    added Headedr Test

--- src/components/Content/Content.test.tsx
+++ src/components/Content/Content.test.tsx
@@ -2,6 +2,7 @@ import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { describe, expect, test } from "vitest";
+
 import Content from "./Content";
 
 describe("Counter Component", () => {
---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+import { describe, expect, test } from "vitest";
+
+import Header from "./Header";
+
+describe("Header Component", () => {
+  test('should render the component with header text', async () => {
+    render(<Header />);
+    expect(screen.getByText(/To-do list/i)).toBeInTheDocument();
+  });
+});
